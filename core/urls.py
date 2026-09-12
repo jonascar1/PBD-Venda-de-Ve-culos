601a71b55fa39b09adbffbf42ecd7a307fa54503
Dowglas Hademar <dowglashademar0@gmail.com>
2026-09-12 20:28:33 -0300

    Mexendo no front

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from . import views_usuarios as views
+from . import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("accounts/login/", views.login_view, name="login"),
+    path("accounts/logout/", views.logout_view, name="logout"),
     path("painel/", views.painel_view, name="painel"),
     path("painel/gerente/", views.painel_gerente_view, name="painel_gerente"),
     path("painel/vendedor/", views.painel_vendedor_view, name="painel_vendedor"),
